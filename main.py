9a8384ecf1caa9c3d083e31f5ea9e6a18eebecd4
gnakatak <gabrielparrini@id.uff.com>
2025-03-11 02:43:32 -0300

    fixing file related bugs

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def main():
         if model.file_type(table):
             # Read the file, transform in csv and trasnform in a DataFrame
             df = model.file_to_csv(table)
-            
+            # ADd new column to the DataFrame
             df = model.add_new_column(df)
-
+            # Edit the table
             new_table = model.table_editor(df)
-
+            # transform in csv and encode it 
             encoded_table = model.csv_encode(new_table)
-
+            # New table name
             table_name = model.new_file_specs()
-
+            # Download the table
             model.download_to_csv(encoded_table,table_name)
 
 if __name__ == '__main__':
